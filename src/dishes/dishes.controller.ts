9c9a8b6d6e7e948788582fa89ae2ef45167938cb
Apoquinto <RCLlanesApo@gmail.com>
2023-05-10 18:14:16 -0600

    feat(dishes): Add get dishes endpoint

--- src/dishes/dishes.controller.ts
+++ src/dishes/dishes.controller.ts
@@ -1,13 +1,19 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateDishDTO } from './dto/create-dish.dto';
 import { DishesService } from './dishes.service';
+import { Dish } from './dish.entity';
 
 @Controller('dishes')
 export class DishesController {
   constructor(private dishesService: DishesService) {}
 
+  @Get()
+  getUsers(): Promise<Dish[]> {
+    return this.dishesService.getAllDishes();
+  }
+
   @Post()
-  createDish(@Body() newDish: CreateDishDTO) {
+  createDish(@Body() newDish: CreateDishDTO): Promise<Dish> {
     return this.dishesService.createDish(newDish);
   }
 }
