000e5e3329da8827244b385bc96674fd94eea61e
Apoquinto <RCLlanesApo@gmail.com>
2023-05-11 11:54:04 -0600

    feat(categories): add create categories endpoint

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpException, Post } from '@nestjs/common';
+import { CategoriesService } from './categories.service';
+import { Categorie } from './categorie.entity';
+import { CreateCategorieDTO } from './dto/create-categorie.dto';
 
 @Controller('categories')
-export class CategoriesController {}
+export class CategoriesController {
+  constructor(private categorieService: CategoriesService) {}
+
+  @Post('')
+  createCategorie(
+    @Body() newCategorie: CreateCategorieDTO,
+  ): Promise<Categorie | HttpException> {
+    return this.categorieService.createCategorie(newCategorie);
+  }
+}
