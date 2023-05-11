fa13bef726f34ac163ea6f796c2dd85a12f4bd3f
Apoquinto <RCLlanesApo@gmail.com>
2023-05-10 18:06:48 -0600

    feat(dishes): add create dish service and DTO

--- src/dishes/dishes.service.ts
+++ src/dishes/dishes.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { Dish } from './dish.entity';
+import { CreateDishDTO } from './dto/create-dish.dto';
 
 @Injectable()
-export class DishesService {}
+export class DishesService {
+  constructor(
+    @InjectRepository(Dish) private dishRepository: Repository<Dish>,
+  ) {}
+
+  createDish(dish: CreateDishDTO) {
+    const newDish = this.dishRepository.create(dish);
+    return this.dishRepository.save(newDish);
+  }
+}
---
+++ src/dishes/dto/create-dish.dto.ts
@@ -0,0 +1,5 @@
+export class CreateDishDTO {
+  name: string;
+  description?: string;
+  price: number;
+}
