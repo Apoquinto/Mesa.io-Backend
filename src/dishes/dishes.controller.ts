0fb60e4e41122cb82400ba13cd03e3c86a64b630
Apoquinto <RCLlanesApo@gmail.com>
2023-05-10 18:09:22 -0600

    feat(dishes): add new dish endpoint to controller

--- src/dishes/dishes.controller.ts
+++ src/dishes/dishes.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateDishDTO } from './dto/create-dish.dto';
+import { DishesService } from './dishes.service';
 
 @Controller('dishes')
-export class DishesController {}
+export class DishesController {
+  constructor(private dishesService: DishesService) {}
+
+  @Post()
+  createDish(@Body() newDish: CreateDishDTO) {
+    return this.dishesService.createDish(newDish);
+  }
+}
