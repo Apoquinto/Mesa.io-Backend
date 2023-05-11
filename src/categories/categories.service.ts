a223386b203823925bb84e033ed2de18d61a93b3
Apoquinto <RCLlanesApo@gmail.com>
2023-05-11 11:53:44 -0600

    feat(categories): add categories service dto

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,4 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { Categorie } from './categorie.entity';
+import { CreateCategorieDTO } from './dto/create-categorie.dto';
 
 @Injectable()
-export class CategoriesService {}
+export class CategoriesService {
+  constructor(
+    @InjectRepository(Categorie)
+    private categorieRepository: Repository<Categorie>,
+  ) {}
+
+  async createCategorie(
+    categorie: CreateCategorieDTO,
+  ): Promise<Categorie | ConflictException> {
+    categorie.name = categorie.name.toLowerCase().trim();
+    const categorieFound = await this.findCategorieByName(categorie.name);
+    if (categorieFound)
+      return new ConflictException(
+        `Unable to create the categorie. The selected name '${categorie.name}' is already in use. Please choose a different name for the categorie`,
+      );
+    const newCategorie = this.categorieRepository.create(categorie);
+    return this.categorieRepository.save(newCategorie);
+  }
+
+  async findCategorieByName(name: string): Promise<Categorie | null> {
+    return this.categorieRepository.findOne({ where: { name } });
+  }
+}
