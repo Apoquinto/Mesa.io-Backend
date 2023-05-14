b1ece8d081c9794123cb1ad9318b121dfdd6b2ff
Apoquinto <RCLlanesApo@gmail.com>
2023-05-14 11:03:52 -0600

    fix(Exceptions): change conflic execption message

--- src/shared/exceptions/CreateConflicException.ts
+++ src/shared/exceptions/CreateConflicException.ts
@@ -13,5 +13,5 @@ export const createConflicException = (
   value: string,
 ): ConflictException =>
   new ConflictException(
-    `Unable to create the dish. The selected ${field} '${value}' is already in use. Please choose a different ${field} for the ${entity}.`,
+    `Unable to create the ${entity}. The selected ${field} '${value}' is already in use. Please choose a different ${field} for the ${entity}.`,
   );
