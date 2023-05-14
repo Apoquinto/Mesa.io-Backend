f7c4672c9f2a36323335676a8ae1a88adfb2b2dc
Apoquinto <RCLlanesApo@gmail.com>
2023-05-14 00:47:35 -0600

    feat(users): add update endpoint

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,10 +6,13 @@ import {
   Param,
   ParseIntPipe,
   Post,
+  Put,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDTO } from './dto/create-user.dto';
 import { User } from './user.entity';
+import { UpdateUserDTO } from './dto/update-user.dto';
+import { DataResponse } from 'src/shared/responses/DataResponse/DataResponse';
 
 @Controller('users')
 export class UsersController {
@@ -30,5 +33,13 @@ export class UsersController {
   @Post('')
   createUser(@Body() user: CreateUserDTO): Promise<User | HttpException> {
     return this.usersService.createUser(user);
+  }
+
+  @Put(':id')
+  updateUser(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() user: UpdateUserDTO,
+  ): Promise<DataResponse<User> | HttpException> {
+    return this.usersService.updateUser(id, user);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -10,6 +10,9 @@ import { User } from './user.entity';
 import { CreateUserDTO } from './dto/create-user.dto';
 import { createConflicException } from 'src/shared/exceptions/CreateConflicException';
 import { createNotFoundException } from 'src/shared/exceptions/CreateNotFoundException';
+import { UpdateUserDTO } from './dto/update-user.dto';
+import { DataResponse } from 'src/shared/responses/DataResponse/DataResponse';
+import { createDataResponse } from 'src/shared/responses/DataResponse/createDataResponse';
 
 @Injectable()
 export class UsersService {
@@ -22,7 +25,7 @@ export class UsersService {
   }
 
   async getUserById(id: number): Promise<User | NotFoundException> {
-    const foundUser = this.userRepository.findOne({
+    const foundUser = await this.userRepository.findOne({
       where: { id },
     });
     if (!foundUser) return createNotFoundException('user', id);
@@ -31,11 +34,26 @@ export class UsersService {
 
   async createUser(user: CreateUserDTO): Promise<User | ConflictException> {
     user.name = user.name.toLowerCase().trim();
-    const foundUser = this.userRepository.findOne({
+    const foundUser = await this.userRepository.findOne({
       where: { name: user.name },
     });
     if (!foundUser) return createConflicException('user', 'name', user.name);
     const newUser = this.userRepository.create(user);
     return this.userRepository.save(newUser);
+  }
+
+  async updateUser(
+    id: number,
+    user: UpdateUserDTO,
+  ): Promise<DataResponse<User> | NotFoundException> {
+    const foundUser = await this.userRepository.findOne({
+      where: { id },
+    });
+    if (!foundUser) return createNotFoundException('user', id);
+    const updatedUser = await this.userRepository.save({
+      ...foundUser,
+      ...user,
+    });
+    return createDataResponse('user', 'updated', id, updatedUser);
   }
 }
