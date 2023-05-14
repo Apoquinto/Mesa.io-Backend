220c5ac5141c2e82f87b64f73ac757e0385c046d
Apoquinto <RCLlanesApo@gmail.com>
2023-05-14 11:09:15 -0600

    feat(Auth): add signIn endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+
+import { AuthService } from './auth.service';
+import { SignInDTO } from './dto/SignIn.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('signIn')
+  signIn(@Body() credentials: SignInDTO) {
+    return this.authService.signIn(credentials);
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+
+import { UsersService } from 'src/users/users.service';
+import { SignInDTO } from './dto/SignIn.dto';
+import { User } from 'src/users/user.entity';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+
+  async signIn(
+    credentials: SignInDTO,
+  ): Promise<User | NotFoundException | UnauthorizedException> {
+    try {
+      const foundUser = await this.usersService.getUserByName(
+        credentials.username,
+      );
+      if (foundUser.password != credentials.password)
+        return new UnauthorizedException(
+          'Invalid username or password. Please check your credentials and try again.',
+        );
+      return foundUser;
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        return new NotFoundException(
+          'Sorry, the user you are trying to reach does not exist. Please check the username and try again.',
+        );
+      }
+    }
+  }
+}
