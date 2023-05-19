f618df7aa0769ddef8903dc865ed4973853788a9
Apoquinto <RCLlanesApo@gmail.com>
2023-05-18 23:11:32 -0600

    feat(dishes): add dishes validations

--- src/dishes/dto/create-dish.dto.ts
+++ src/dishes/dto/create-dish.dto.ts
@@ -1,3 +1,11 @@
+import {
+  IsArray,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+
 /**
  * Class representing the data for creating a dish.
  * @class
@@ -7,20 +15,28 @@ export class CreateDishDTO {
    * The name of the dish.
    * @type {string}
    */
+  @IsNotEmpty()
+  @IsString()
   name: string;
   /**
    * The description of the dish (optional).
    * @type {string | undefined}
    */
+  @IsOptional()
+  @IsString()
   description?: string;
   /**
    * The price of the dish.
    * @type {number}
    */
+  @IsNotEmpty()
+  @IsNumber()
   price: number;
   /**
    * The IDs of the categories linked to the dish.
    * @type {number[]}
    */
+  @IsNotEmpty()
+  @IsArray()
   categories: number[];
 }
--- src/dishes/dto/update-dish.dto.ts
+++ src/dishes/dto/update-dish.dto.ts
@@ -1,6 +1,14 @@
+import { IsNotEmpty, IsOptional, IsString, IsNumber } from 'class-validator';
+
 export class UpdateDishDTO {
+  @IsOptional()
+  @IsNotEmpty()
+  @IsString()
   name?: string;
+  @IsOptional()
+  @IsString()
   description?: string;
+  @IsOptional()
+  @IsNumber()
   price?: number;
-  dishThumbnailURL?: string;
 }
